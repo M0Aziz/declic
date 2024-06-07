5a5aa8a1e16fa9592c16b182abb655a82acff9fc
Aziz Chouchi <98626462+M0Aziz@users.noreply.github.com>
2024-06-07 18:25:11 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
  
   try {
-    await mongoose.connect('mongodb://localhost:27017/declic', {
+    await mongoose.connect('mongodb://mongodb:27017/declic', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
